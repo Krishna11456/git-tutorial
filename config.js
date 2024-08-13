f58b06013fa3408a607bf0b86d17aa112ca7dfc6
krishna <hrraj3456@gmail.com>
2024-08-13 20:31:24 +0530

    Version 1

---
+++ config.js
@@ -0,0 +1,6 @@
+console.log('index.js');
+console.log('version1');
+
+
+//The staging area is a space where you prepare your changes before committing them. It's like a buffer or a checklist of what you want to include in the next version (commit).
+//When you use git add, you move changes from the working area to the staging area. This tells Git, "These are the changes I want to include in the next commit."

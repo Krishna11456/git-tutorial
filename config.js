706c7ef5ec82ded352a494960c9e0ca7146b12f0
krishna <hrraj3456@gmail.com>
2024-08-14 18:07:37 +0530

    Version 13

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 console.log('index.js');
-console.log('version12');
+console.log('version13');
 
 
 //The staging area is a space where you prepare your changes before committing them. It's like a buffer or a checklist of what you want to include in the next version (commit).
